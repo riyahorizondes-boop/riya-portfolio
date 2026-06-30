8246037485e826b1a6859a7351ff0247d4012f01
Riya <riyahorizon.des@gmail.com>
2026-06-30 17:50:22 +0530

    RESPOSIVE CARDS

--- js/animations.js
+++ js/animations.js
@@ -3,24 +3,31 @@ const heroVideo = document.getElementById("hero-video");
 const polaroid = document.querySelector(".polaroid");
 const pencil = document.querySelector(".pencil");
 const brush = document.querySelector(".brush");
+const polaroidWrapper = document.querySelector(".polaroid-wrapper");
 
 heroVideo.addEventListener("ended", () => {
 
     polaroid.style.animation =
         "polaroidDrop .9s cubic-bezier(.22,1.25,.3,1) forwards";
 
-    setTimeout(() => {
+        setTimeout(() => {
 
-        pencil.style.animation =
-            "pencilSwing .8s cubic-bezier(.22,1,.36,1) forwards";
+            polaroidWrapper.classList.add("active");
 
-    },300);
+        }, 900);
 
-    setTimeout(() => {
+        setTimeout(() => {
 
-        brush.style.animation =
-            "brushSlide .55s ease forwards";
+            pencil.style.animation =
+                "pencilSwing .8s cubic-bezier(.22,1,.36,1) forwards";
 
-    },520);
+        },300);
+
+        setTimeout(() => {
+
+            brush.style.animation =
+                "brushSlide .55s ease forwards";
+
+        },520);
 
 });
